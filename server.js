66302c196bed08212a9e543023f4a1a8ae757a4f
Sanskruti Babar <sanskrutibabar1507@gmail.com>
2026-03-22 15:10:42 +0530

    fixed railway port binding

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ const start = async () => {
 
   const PORT = process.env.PORT || 3000;
 
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
+  app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 };
 
 start();
